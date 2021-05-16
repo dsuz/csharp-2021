61bfe0181494d039ed9c7db69fc8a2a902c3ff8c
D. Suzuki <bboydaisuke@gmail.com>
2021-05-17 01:53:24 +0900

    4週目の内容を追加した

---
+++ CSharp1-1-4/CSharp1-1-4/Class.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class Vector
+{
+    // メンバ変数（フィールド）
+    public double x; // プロパティ = public なメンバ変数。外部から読み書きできる。
+    public double y; // プロパティ = public なメンバ変数。外部から読み書きできる。
+
+    // コンストラクター（デフォルトコンストラクター）
+    public Vector()
+    {
+    }
+
+    /// <summary>
+    /// 長さを求めて出力する
+    /// </summary>
+    /// <returns>長さ</returns>
+    public void ShowLength()    // public なので外部から呼び出せる
+    {
+        double length = Math.Sqrt(x * x + y * y);
+        Console.WriteLine("x: {0}, y: {1} の時、長さは {2} です", x, y, length);
+    }
+}
+
+class Person
+{
+    // メンバ変数（フィールド）
+    public string name;             // プロパティ = public なメンバ変数。外部から読み書きできる。
+    private int birthYear = 1900;   // private なメンバ変数。外部からは見えないし操作できない。
+    private int birthMonth = 1;     // private なメンバ変数。外部からは見えないし操作できない。
+    private int birthDay = 1;       // private なメンバ変数。外部からは見えないし操作できない。
+
+    /// <summary>
+    /// コンストラクター（引数付きコンストラクター）
+    /// </summary>
+    /// <param name="name">名前</param>
+    /// <param name="birthYear">誕生年</param>
+    /// <param name="birthMonth">誕生月</param>
+    /// <param name="birthDay">誕生日</param>
+    public Person(string name, int birthYear, int birthMonth, int birthDay)
+    {
+        this.name = name;   // this を使うことで、メンバ変数とパラメータ変数が同じ名前なのを区別している
+        this.birthYear = birthYear;
+        this.birthMonth = birthMonth;
+        this.birthDay = birthDay;
+    }
+
+    /// <summary>
+    /// 年齢を返す
+    /// </summary>
+    /// <returns>年齢</returns>
+    public int GetAge()    // public なので外部から呼び出せる
+    {
+        int age = CalcAge();
+        return age;
+    }
+
+    /// <summary>
+    /// 年齢を計算する
+    /// </summary>
+    /// <returns></returns>
+    private int CalcAge()   // private なので外部から呼び出せない
+    {
+        string strToday = DateTime.Now.ToString("yyyyMMdd");
+        int intToday = int.Parse(strToday);
+
+        string strBirthday = this.birthYear.ToString("0000") + this.birthMonth.ToString("00") + this.birthDay.ToString("00");
+        int intBirthday = int.Parse(strBirthday);
+
+        int age = (intToday - intBirthday) / 10000;
+        return age;
+    }
+}
---
+++ CSharp1-1-4/CSharp1-1-4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Vector v1 = new Vector();
+        v1.x = 1;
+        v1.y = 2;
+        v1.ShowLength();
+
+        v1.x = 3;
+        v1.y = 4;
+        v1.ShowLength();
+
+        Vector v2 = new Vector();
+        v2.x = 4;
+        v2.y = 5;
+        v2.ShowLength();
+
+        Wait();
+
+        /*
+         * ここで以下をやってみましょう
+         * https://paiza.jp/works/cs/primer/beginner-cs7/21001/1 の演習 1
+         * */
+
+        Person shohei = new Person("Shohei Ohtani", 1994, 7, 5);
+        int age1 = shohei.GetAge();
+        Console.WriteLine(shohei.name + " さんの年齢は " + age1 + " 歳です。");
+
+        Person p = new Person("Roki Sasaki", 2001, 11, 3);
+        Console.WriteLine("{0} さんの年齢は {1} 歳です。", p.name, p.GetAge());
+
+        Wait();
+
+        /*
+         * ここで以下をやってみましょう
+         * https://paiza.jp/works/cs/primer/beginner-cs7/21002/1 の演習 1
+         * https://paiza.jp/works/cs/primer/beginner-cs7/21004/1 の演習 1
+         * */
+
+
+
+    }
+
+    static void Wait()  // void は「戻り値なし」という意味です。static は、ここでは "static な関数 Main から呼ばれたから" 必要です。コンソールアプリケーションの場合は関数に static を付ける必要があります。
+    {
+        Console.Write("Enter キーを押すと次に進みます。Ctrl + C で実行を中止できます...");
+        Console.ReadLine();
+    }
+}
