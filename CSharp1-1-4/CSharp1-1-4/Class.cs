2d0a63ba96555d68b91275a0bfa84469c7d1898e
D. Suzuki <bboydaisuke@gmail.com>
2021-05-18 22:52:23 +0900

    4週目の内容に追記した

--- CSharp1-1-4/CSharp1-1-4/Class.cs
+++ CSharp1-1-4/CSharp1-1-4/Class.cs
@@ -2,33 +2,34 @@
 
 class Vector
 {
-    // メンバ変数（フィールド）
+    // メンバー変数（フィールド、クラスフィールドとも言う）
     public double x; // プロパティ = public なメンバ変数。外部から読み書きできる。
     public double y; // プロパティ = public なメンバ変数。外部から読み書きできる。
 
-    // コンストラクター（デフォルトコンストラクター）
+    // コンストラクター（デフォルトコンストラクター、省略可能）
     public Vector()
     {
+        Console.WriteLine("コンストラクターが呼び出された");
     }
 
     /// <summary>
     /// 長さを求めて出力する
     /// </summary>
     /// <returns>長さ</returns>
-    public void ShowLength()    // public なので外部から呼び出せる
+    public void ShowLength()    // public なので外部から呼び出せる。メンバー関数・メソッド・メンバメソッドと呼ばれる
     {
-        double length = Math.Sqrt(x * x + y * y);
-        Console.WriteLine("x: {0}, y: {1} の時、長さは {2} です", x, y, length);
+        double length = Math.Sqrt(x * x + y * y);   // ローカル変数（関数内からのみ使える変数）
+        Console.WriteLine("x: {0}, y: {1} の時、長さは {2} です", this.x, this.y, length);  // this は「このクラスのインスタンス」を表す（省略可能）
     }
 }
 
 class Person
 {
-    // メンバ変数（フィールド）
+    // メンバー変数（フィールド、クラスフィールドとも言う）
     public string name;             // プロパティ = public なメンバ変数。外部から読み書きできる。
-    private int birthYear = 1900;   // private なメンバ変数。外部からは見えないし操作できない。
-    private int birthMonth = 1;     // private なメンバ変数。外部からは見えないし操作できない。
-    private int birthDay = 1;       // private なメンバ変数。外部からは見えないし操作できない。
+    private int birthYear = 1900;   // private なメンバ変数。外部からは見えないし操作できない。省略可能。
+    private int birthMonth = 1;     // private なメンバ変数。外部からは見えないし操作できない。省略可能。
+    private int birthDay = 1;       // private なメンバ変数。外部からは見えないし操作できない。省略可能。
 
     /// <summary>
     /// コンストラクター（引数付きコンストラクター）
@@ -39,7 +40,9 @@ class Person
     /// <param name="birthDay">誕生日</param>
     public Person(string name, int birthYear, int birthMonth, int birthDay)
     {
-        this.name = name;   // this を使うことで、メンバ変数とパラメータ変数が同じ名前なのを区別している
+        // ここの this は省略できない
+        // this を使うことで、メンバ変数とパラメータ変数が同じ名前なのを区別しているため
+        this.name = name;
         this.birthYear = birthYear;
         this.birthMonth = birthMonth;
         this.birthDay = birthDay;
@@ -51,7 +54,7 @@ class Person
     /// <returns>年齢</returns>
     public int GetAge()    // public なので外部から呼び出せる
     {
-        int age = CalcAge();
+        int age = this.CalcAge();   // ローカル変数（関数内からのみ使える変数）
         return age;
     }
 
@@ -59,8 +62,10 @@ class Person
     /// 年齢を計算する
     /// </summary>
     /// <returns></returns>
-    private int CalcAge()   // private なので外部から呼び出せない
+    private int CalcAge()   // private なので外部から呼び出せない（private は省略可能）
     {
+        // ここの処理は現段階では理解しなくてもよい
+        // なお、this は省略している
         string strToday = DateTime.Now.ToString("yyyyMMdd");
         int intToday = int.Parse(strToday);
 
@@ -69,5 +74,35 @@ class Person
 
         int age = (intToday - intBirthday) / 10000;
         return age;
+    }
+}
+
+class StaticTest
+{
+    public static float Pi = 3.14f;
+    public static int count = 0;
+
+    /// <summary>
+    /// int の配列をもらって、その合計を返す
+    /// </summary>
+    /// <returns></returns>
+    public static int Sum(int[] numbers)
+    {
+        int sum = 0;
+
+        foreach (int n in numbers)
+        {
+            sum += n;
+        }
+
+        return sum;
+    }
+
+    /// <summary>
+    /// コンストラクター
+    /// </summary>
+    public StaticTest()
+    {
+        count++;
     }
 }
--- CSharp1-1-4/CSharp1-1-4/Program.cs
+++ CSharp1-1-4/CSharp1-1-4/Program.cs
@@ -4,7 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Vector v1 = new Vector();
+        Console.WriteLine("Class01 を呼び出します...");
+        Class01();
+        Wait();
+
+        Console.WriteLine("Class02 を呼び出します...");
+        Class02();
+        Wait();
+
+        Console.WriteLine("UseBuiltInClass を呼び出します...");
+        UseBuiltInClass();
+        Wait();
+
+        Console.WriteLine("Static01 を呼び出します...");
+        Static01();
+        Wait();
+
+        Console.WriteLine("UseBuiltInStatic を呼び出します...");
+        UseBuiltInStatic();
+        Wait();
+    }
+
+    /// <summary>
+    /// クラスを定義し、インスタンスを作って機能を呼び出す
+    /// </summary>
+    static void Class01()
+    {
+        Vector v1 = new Vector();   // 何もない状態から新たな「インスタンス（「オブジェクト」と呼ばれる時もある）」を作る時は new を使う。
         v1.x = 1;
         v1.y = 2;
         v1.ShowLength();
@@ -18,13 +44,17 @@ class Program
         v2.y = 5;
         v2.ShowLength();
 
-        Wait();
-
         /*
          * ここで以下をやってみましょう
          * https://paiza.jp/works/cs/primer/beginner-cs7/21001/1 の演習 1
          * */
+    }
 
+    /// <summary>
+    /// 引数つきコンストラクターと戻り値を返すメソッドを使う
+    /// </summary>
+    static void Class02()
+    {
         Person shohei = new Person("Shohei Ohtani", 1994, 7, 5);
         int age1 = shohei.GetAge();
         Console.WriteLine(shohei.name + " さんの年齢は " + age1 + " 歳です。");
@@ -32,21 +62,65 @@ class Program
         Person p = new Person("Roki Sasaki", 2001, 11, 3);
         Console.WriteLine("{0} さんの年齢は {1} 歳です。", p.name, p.GetAge());
 
-        Wait();
-
         /*
          * ここで以下をやってみましょう
          * https://paiza.jp/works/cs/primer/beginner-cs7/21002/1 の演習 1
          * https://paiza.jp/works/cs/primer/beginner-cs7/21004/1 の演習 1
          * */
-
-
-
     }
 
-    static void Wait()  // void は「戻り値なし」という意味です。static は、ここでは "static な関数 Main から呼ばれたから" 必要です。コンソールアプリケーションの場合は関数に static を付ける必要があります。
+    /// <summary>
+    /// 最初から用意されている（組み込み、built-in）クラスを使う
+    /// </summary>
+    static void UseBuiltInClass()
     {
-        Console.Write("Enter キーを押すと次に進みます。Ctrl + C で実行を中止できます...");
+        // string もクラスである（int, float, bool や配列も実はクラスである）
+        string str = "Microsoft Visual Studio is an integrated development environment (IDE) from Microsoft.";
+        Console.WriteLine("{0} の文字数は {1} です", str, str.Length);
+        Console.WriteLine("{0} の単語数は {1} です", str, str.Split(new string[] { " " }, StringSplitOptions.None).Length);
+
+        // 日付や日時を表すための DateTime　というクラスがある
+        DateTime dt = new DateTime(2012, 5, 5); // 2012年5月5日
+        dt = dt.AddDays(30);     // こういった「日付操作に都合のよい機能」が用意されている
+        dt = dt.AddMonths(10);
+        Console.WriteLine(dt.ToString("yyyy年MM月dd日"));  // ToString の引数は日付書式指定文字列です。参照: https://csharp.sql55.com/cookbook/format-datetime.php
+    }
+
+    /// <summary>
+    /// static 関数、static プロパティを使う
+    /// </summary>
+    static void Static01()
+    {
+        Console.WriteLine(StaticTest.Pi);
+        int[] numbers = { 5, 3, 2, 6, 9, 10 };
+        int sum = StaticTest.Sum(numbers);  // static な関数はインスタンスを作るのではなく「クラス名.関数」で呼び出す
+        Console.WriteLine(sum);
+
+        // static なプロパティは「各インスタンスに依存しない」値を返すことができる
+        StaticTest st1 = new StaticTest(), st2 = new StaticTest(), st3 = new StaticTest();
+        Console.WriteLine("StaticTest クラスのインスタンスは {0} 個あります", StaticTest.count);
+
+        /*
+         * ここで以下をやってみましょう
+         * https://paiza.jp/works/cs/primer/beginner-cs7/21007/1
+         * */
+    }
+
+    /// <summary>
+    /// 組み込みの static 関数、static プロパティを使う
+    /// </summary>
+    static void UseBuiltInStatic()
+    {
+        // インスタンスを使わず、クラス名.プロパティ とか クラス名.関数 で呼び出せていることに注目する
+        Console.WriteLine("今日は {0} です", DateTime.Now.ToString("yyyy年MM月dd日"));
+        Console.WriteLine("π = {0:f5} です", Math.PI);    // {0:f5} は「小数点以下5桁まで表示」という書式指定文字列です
+        Console.WriteLine("58 と 72 では {0} の方が大きい", Math.Max(58, 72));
+        Console.WriteLine("58 と 72 では {0} の方が小さい", Math.Min(58, 72));
+    }
+
+    static void Wait()
+    {
+        Console.Write("Enter キーを押すと次に進みます。Ctrl + C で実行を中止できます...\n");
         Console.ReadLine();
     }
 }
