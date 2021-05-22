567556bdc6cb0f20d7d0a0b0fcf2e12357743ba5
D. Suzuki <bboydaisuke@gmail.com>
2021-05-23 00:10:23 +0900

    画面に説明のテキストを追加し、コードにコメントを追加した

--- CSharp/Assets/_05 Basic/Scripts/StarController.cs
+++ CSharp/Assets/_05 Basic/Scripts/StarController.cs
@@ -1,20 +1,32 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// Star のオブジェクトを制御するコンポーネント
+/// 画面下に消えたら自分自身を消す（正式な言い方としては「破棄する」という）
+/// </summary>
 public class StarController : MonoBehaviour
 {
     void Update()
     {
+        // 画面下に消えたら（y 座標が -10 より小さくなったら）自分自身を破棄する
         if (this.transform.position.y < -10f)
         {
             Destroy(this.gameObject);
         }
     }
 
+    /// <summary>
+    /// 他のコライダーと衝突した時に呼び出される
+    /// </summary>
+    /// <param name="collision"></param>
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // 衝突した相手に "Destructable" タグが付いていたら
         if (collision.gameObject.tag == "Destructable")
         {
+            // 衝突相手を破棄して
             Destroy(collision.gameObject);
+            // 自分自身も破棄する
             Destroy(this.gameObject);
         }
     }
