d1b3b6dbce4d4d38b23cd5eac11f0ea6d147654f
D. Suzuki <bboydaisuke@gmail.com>
2021-07-01 23:34:57 +0900

    2-3週目の内容を追加した

---
+++ CSharp/Assets/_2-3 enum/HumanControllerTopDown2D.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// プレイヤーを動かすためのコンポーネント
+/// 状態は「正常、毒、麻痺、死」の 4 種類あり、それぞれ以下のような効果がある。
+/// 毒: ライフが徐々に減る
+/// 麻痺: 移動速度が落ちる
+/// 死: 何もできなくなる（動けない）
+/// 正常: 普通に移動でき、ライフも減らない
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer))]
+public class HumanControllerTopDown2D : MonoBehaviour
+{
+    /// <summary>ライフの最大値</summary>
+    [SerializeField] float m_maxLife = 100;
+    /// <summary>移動速度</summary>
+    [SerializeField] float m_speed = 1f;
+    /// <summary>毒の時にどれくらいライフが減るか</summary>
+    [SerializeField] float m_lifeReduceSpeedOnPoisoned = 1f;
+    /// <summary>麻痺の時にどれくらい移動速度が落ちるか</summary>
+    [SerializeField] float m_speedReductionRatioOnParalyzed = 0.5f;
+    [SerializeField] Slider m_lifeGauge = default;
+    Rigidbody2D m_rb = default;
+    Animator m_anim = default;
+    SpriteRenderer m_sprite = default;
+    float m_life = 0;
+    bool m_isPoisoned = false;
+    bool m_isParalyzed = false;
+    bool m_isDead = false;
+    PlayerState m_state = PlayerState.Normal;
+
+    void Start()
+    {
+        m_life = m_maxLife;
+        m_rb = GetComponent<Rigidbody2D>();
+        m_anim = GetComponent<Animator>();
+        m_sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // 入力検出と移動
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Vector2 dir = new Vector2(h, v).normalized;
+
+        // 状態判定
+        if (m_isParalyzed)
+        {
+            m_rb.velocity = dir * m_speed * m_speedReductionRatioOnParalyzed;
+        }
+        else if (m_isDead)
+        {
+            // 何もできなくなる
+            m_rb.velocity = Vector2.zero;
+        }
+        else
+        {
+            m_rb.velocity = dir * m_speed;
+
+            if (m_isPoisoned)
+            {
+                m_life -= m_lifeReduceSpeedOnPoisoned * Time.deltaTime;
+            }
+        }
+
+        // 生死判定
+        if (m_life < 0 && m_isDead == false)
+        {
+            m_isDead = true;
+            m_sprite.color = Color.red;
+        }
+
+        // ゲージ処理
+        if (m_lifeGauge)
+        {
+            m_lifeGauge.value = m_life / m_maxLife;
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (m_anim)
+        {
+            if (m_rb.velocity.magnitude > 0)
+            {
+                m_anim.Play("Walk");
+            }
+            else
+            {
+                m_anim.Play("Idle");
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Poison")
+        {
+            m_isPoisoned = true;
+            m_isParalyzed = false;
+            m_sprite.color = Color.magenta;
+        }
+        else if (collision.gameObject.tag == "Paralyze")
+        {
+            m_isPoisoned = false;
+            m_isParalyzed = true;
+            m_sprite.color = Color.yellow;
+        }
+        else
+        {
+            m_isPoisoned = false;
+            m_isParalyzed = false;
+            m_sprite.color = Color.white;
+        }
+    }
+}
+
+/// <summary>
+/// プレイヤーの状態を表す
+/// </summary>
+enum PlayerState
+{
+    /// <summary>通常</summary>
+    Normal,
+    /// <summary>毒 ライフが減る</summary>
+    Poisoned,
+    /// <summary>麻痺 移動が遅くなる</summary>
+    Paralyzed,
+    /// <summary>死</summary>
+    Dead,
+}
