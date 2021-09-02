e710289eb12ca2512ecf5a75d5d97a7adf9516dd
D. Suzuki <bboydaisuke@gmail.com>
2021-09-02 22:48:04 +0900

    第2ターム7週目 課題1の解答

--- CSharp/Assets/_2-7 Interface/1 Pause/PaddleController.cs
+++ CSharp/Assets/_2-7 Interface/1 Pause/PaddleController.cs
@@ -4,11 +4,13 @@
 /// パドルを制御するコンポーネント
 /// </summary>
 [RequireComponent(typeof(Rigidbody2D))]
-public class PaddleController : MonoBehaviour
+public class PaddleController : MonoBehaviour, IPause
 {
     /// <summary>パドルが動く速さ</summary>
     [SerializeField] float m_speed = 1f;
     Rigidbody2D m_rb = default;
+    /// <summary>一時停止フラグ（true の時は停止している）</summary>
+    bool m_isPaused = false;
 
     void Start()
     {
@@ -17,8 +19,21 @@ public class PaddleController : MonoBehaviour
 
     void Update()
     {
+        if (m_isPaused) return;
+
         // 入力を受け付けて、パドルを動かす
         float h =Input.GetAxisRaw("Horizontal");
         m_rb.velocity = Vector2.right * h * m_speed;
+    }
+
+    void IPause.Pause()
+    {
+        m_isPaused = true;
+        m_rb.velocity = Vector2.zero;   // こうしないと「移動しながら ESC キーを押した」時に停止しない
+    }
+
+    void IPause.Resume()
+    {
+        m_isPaused = false;
     }
 }
