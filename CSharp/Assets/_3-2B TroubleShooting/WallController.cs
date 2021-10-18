87a836b6ad0c0e9839b51f024a8705dc7710b07f
D. Suzuki <bboydaisuke@gmail.com>
2021-10-18 23:49:49 +0900

    第3ターム2週目B の内容と教材を追加した

---
+++ CSharp/Assets/_3-2B TroubleShooting/WallController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 壁を制御するコンポーネント
+/// 壁のオブジェクトにアタッチして使う。
+/// 壁を一定速度で左に動かし、ある程度左に行ったら破棄する。
+/// </summary>
+public class WallController : MonoBehaviour
+{
+    /// <summary>左に動く速さ</summary>
+    [SerializeField] float m_moveSpeed = 1f;
+
+    void Update()
+    {
+        // 一定速度で左に動かす
+        this.transform.Translate(Vector2.left * m_moveSpeed * Time.deltaTime);
+    }
+}
---
+++ CSharp/Assets/_3-2B TroubleShooting/WallGenerator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 一定間隔で壁を生成するコンポーネント
+/// 適当なオブジェクトにアタッチして使う。
+/// 一定間隔で、設定した Wall Prefabs からランダムにプレハブを選び、生成する
+/// </summary>
+public class WallGenerator : MonoBehaviour
+{
+    /// <summary>壁として生成するプレハブ</summary>
+    [SerializeField] GameObject[] m_wallPrefabs = null;
+    /// <summary>壁を生成する間隔（秒）</summary>
+    [SerializeField] float m_wallGenerateInterval = 2f;
+
+    void Start()
+    {
+        GenerateWall();
+    }
+
+    void Update()
+    {
+    }
+
+    /// <summary>
+    /// プレハブにセットした「壁」プレハブのうち、ランダムに一つを生成する
+    /// </summary>
+    void GenerateWall()
+    {
+        GameObject go = Instantiate(m_wallPrefabs[0]);  // プレハブからオブジェクトを生成して、変数 go に入れる
+    }
+}
