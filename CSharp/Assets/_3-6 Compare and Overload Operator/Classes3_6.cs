d5be20959c41a8d3603cc2212256d8a34a90a153
D. Suzuki <bboydaisuke@gmail.com>
2021-12-01 03:21:44 +0900

    第3ターム6週目の内容を追加した

---
+++ CSharp/Assets/_3-6 Compare and Overload Operator/Classes3_6.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+
+/// <summary>
+/// アイテム（装備）データを格納する構造体
+/// ID 順でソートする。つまり ID の値が大きい方が「大きいアイテム」とみなされる。
+/// アイテムとアイテムの和 (+) は合成されたアイテムとなり、ランダムなアイテムとなる。
+/// </summary>
+public struct Gear : IComparable
+{
+    /// <summary>アイテムの ID</summary>
+    public int Id;
+    /// <summary>アイテムの名前</summary>
+    public string Name;
+    /// <summary>アイテムの値段</summary>
+    public int Price;
+    /// <summary>攻撃力</summary>
+    public int Attack;
+    /// <summary>防御力</summary>
+    public int Defence;
+
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public Gear(int id, string name, int price, int attack, int defence)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Price = price;
+        this.Attack = attack;
+        this.Defence = defence;
+    }
+
+    /// <summary>
+    /// 大小関係を ID 順として定義する
+    /// </summary>
+    /// <param name="o">大小関係を比較する相手</param>
+    /// <returns>自分の方が小さい時は -1, 自分の方が大きい時は 1, 同値の時は 0</returns>
+    int IComparable.CompareTo(object o)
+    {
+        Gear other = (Gear) o;
+
+        if (this.Id < other.Id)
+        {
+            return -1;  // 自分の ID が小さい時は「自分の方が小さい」とする
+        }
+        else if (this.Id > other.Id)
+        {
+            return 1;  // 自分の ID が大きい時は「自分の方が大きい」とする
+        }
+
+        return 0;   // ID が同じなら「同じ」とする
+    }
+
+    /// <summary>
+    /// 文字列データに変換する
+    /// </summary>
+    /// <returns>文字列</returns>
+    public override string ToString()
+    {
+        return $"Id: {this.Id}, Name: {this.Name}, Price: {this.Price}, Attack: {this.Attack}, Defence: {this.Defence}";
+    }
+
+    /// <summary>
+    /// Gear の足し算を定義する。
+    /// Gear と Gear を足した場合、「合成」としてランダムな Gear を返す。
+    /// </summary>
+    /// <param name="g1">合成する Gear</param>
+    /// <param name="g2">合成する Gear</param>
+    /// <returns>合成された Gear</returns>
+    public static Gear operator +(Gear g1, Gear g2)
+    {
+        int maxId = DataLoader.GearData.Max(item => item.Id);
+        int id = UnityEngine.Random.Range(1, maxId + 1);
+        Gear newGear = DataLoader.GearData.Where(item => item.Id == id).FirstOrDefault();
+        return newGear;
+    }
+}
---
+++ CSharp/Assets/_3-6 Compare and Overload Operator/DataLoader.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+/// <summary>
+/// アイテムデータを操作するコンポーネント
+/// マスターデータの読み込み、ID を元にしたデータ（クラス）の取得、ができる
+/// </summary>
+public class DataLoader : MonoBehaviour
+{
+    /// <summary>読み込んだアイテムのデータ</summary>
+    static List<Gear> _gearData = new List<Gear>();
+    [SerializeField] TextAsset _gearDataCsv = default;
+
+    /// <summary>
+    /// アイテムデータ（マスターデータ）のリスト
+    /// </summary>
+    public static List<Gear> GearData
+    {
+        get
+        {
+            return _gearData;
+        }
+    }
+
+    /// <summary>
+    /// 指定した CSV ファイルからアイテムデータを読み込む
+    /// </summary>
+    public void LoadGearData()
+    {
+        Debug.Log("アイテムデータ読み込み開始");
+        StringReader sr = new StringReader(_gearDataCsv.text);
+        sr.ReadLine();  // 先頭行はヘッダなのでスキップする
+
+        // 一行ずつ読み込んでインスタンスをリストに追加する
+        while (sr.Peek() != -1)
+        {
+            string line = sr.ReadLine();
+            var d = line.Split(',');
+            Gear gear = new Gear(int.Parse(d[0]), d[1], int.Parse(d[2]), int.Parse(d[3]), int.Parse(d[4]));
+            _gearData.Add(gear);
+        }
+
+        Debug.Log("アイテムデータ読み込み完了");
+    }
+
+    /// <summary>
+    /// アイテムデータをソートし、Console にソート前後の情報を出力する
+    /// ソート順は Gear 構造体に定義されている
+    /// </summary>
+    public void SortGearData()
+    {
+        StringBuilder sb = new StringBuilder(); // 長い文字列を作る時に使うクラス
+        sb.AppendLine("=== ソート前 ===");
+
+        foreach (var i in _gearData)
+        {
+            sb.AppendLine(i.ToString());
+        }
+
+        _gearData.Sort();
+        sb.AppendLine("=== ソート後 ===");
+
+        foreach (var i in _gearData)
+        {
+            sb.AppendLine(i.ToString());
+        }
+
+        Debug.Log(sb.ToString());
+    }
+
+    /// <summary>
+    /// ID を指定してアイテムデータを取得する
+    /// </summary>
+    /// <param name="id">取得したいアイテムの ID</param>
+    /// <returns>アイテムデータのインスタンス</returns>
+    public static Gear GetGear(int id)
+    {
+        return DataLoader.GearData.Where(item => item.Id == id).FirstOrDefault();
+    }
+}
